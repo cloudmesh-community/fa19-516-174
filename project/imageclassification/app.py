2435e251b1a9caa8bce3edb34361e98680f728e4
sahancha <sancha@iu.edu>
2019-12-09 11:34:31 -0500

    image classification

---
+++ project/imageclassification/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import numpy
+import keras
+import requests
+import subprocess
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.IRG
+users = db["Users"]
+
+class Classify(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        url = postedData["url"]
+
+        r = requests.get(url)
+        retJson = {}
+        with open('temp.jpg', 'wb') as f:
+            f.write(r.content)
+            proc = subprocess.Popen('python classify.py --model_dir=. --image_file=./temp.jpg', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            ret = proc.communicate()[0]
+            proc.wait()
+            with open("text.txt") as f:
+                retJson = json.load(f)
+
+        return retJson
+
+api.add_resource(Classify, '/classify')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
---
+++ project/imageclassification/classify.py
@@ -0,0 +1,17 @@
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+
+img = image.load_img(temp.jpg)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+retJson[decode_predictions(preds, top=3)]
+print('Predicted:', decode_predictions(preds, top=3)[0])
+with open("text.txt") as f:
+    json.dump(retJson)
