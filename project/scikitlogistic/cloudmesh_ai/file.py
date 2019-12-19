ee209b835e03b18d1c79c2510a0c7afaf5f285a9
sahancha <sancha@iu.edu>
2019-12-19 13:08:02 -0500

    logistic regression

--- project/scikitlogistic/cloudmesh_ai/file.py
+++ project/scikitlogistic/cloudmesh_ai/file.py
@@ -1,25 +1,23 @@
-import pandas as pd
-from cloudmesh import mongo # ???
+import mongo
+#from cloudmesh import mongo # ???
 from flask import request
-from flask_pymongo import PyMongo
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from project.scikitlogistic.server import data
 
 
 def upload(file=None):
+
     if 'trainfile' in request.files:
         trainfile = request.files['trainfile']
-        mongo.save_file(trainfile.filename, trainfile)
-        mongo.db.users.insert(
-            {'username': request.form.get('username'),
-             'uploadfile_name': trainfile.filename})
-        trainset = mongo.db.users.find_one_or_404({'uploadfile_name': trainfile.filename})
+        data.insert({"file": "trainfile"})
+        #mongo.save_file(trainfile.filename, trainfile)
+        #mongo.db.users.insert({'username': request.form.get('username'),'uploadfile_name': trainfile.filename})
+        trainset = mongo.db.data.find_one_or_404({'uploadfile_name': trainfile.filename})
 
     if 'testfile' in request.files:
         testfile = request.files['testfile']
-        mongo.save_file(testfile.filename, testfile)
-        mongo.db.users.insert({'username': request.form.get('username'), 'uploadfile_name': testfile.filename})
-        testset = mongo.db.users.find_one_or_404({'uploadfile_name': testfile.filename})
+        data.insert({"file": "testfile"})
+        #mongo.save_file(testfile.filename, testfile)
+        #mongo.db.users.insert({'username': request.form.get('username'), 'uploadfile_name': testfile.filename})
+        testset = mongo.db.data.find_one_or_404({'uploadfile_name': testfile.filename})
 
     return 'File uploaded successfully', trainset, testset
