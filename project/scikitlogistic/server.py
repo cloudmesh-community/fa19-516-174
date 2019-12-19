181b159c4a7491df3cea16338260738e2fe0f71c
sahancha <sancha@iu.edu>
2019-12-19 11:00:03 -0500

    NONE

--- project/scikitlogistic/server.py
+++ project/scikitlogistic/server.py
@@ -1,10 +1,10 @@
 import os
-#import pymongo
+import pymongo
 from flask import jsonify, request, render_template, url_for, Flask
-#from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource
 import connexion
-#from flask_pymongo import PyMongo
-#from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from werkzeug.utils import secure_filename, redirect
 
 # http://0.0.0.0:8080/cloudmesh_ai/ui
@@ -17,27 +17,24 @@ db = cmdb.client["cloudmesh_ai"]
 data = db["files"]
 data.insert_one({"gregor": "hallo"})
 
-
-
 # Create the application instance
-app = connexion.FlaskApp(__name__, specification_dir="../logreg/")
+app = connexion.App(__name__, specification_dir="../scikitlogistic/")
 app.add_api("api.yaml")
-
 
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
     msg = {"msg": "It's working!"}
-    return jsonify(msg)
+    return jsonify(msg), render_template("upload.html");
 
 #
 # this is actually being configures in the yaml file and not here in tha app.
 # the app has only starting the server based on the app configuration. other
 # than the home route this should not have any other routes
 #
-@app.route("/upload_file", methods=['POST'])
-def file_upload():
-    return render_template("upload.html");
+#@app.route("/upload_file", methods=['POST'])
+#def file_upload():
+#    return render_template("upload.html");
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
