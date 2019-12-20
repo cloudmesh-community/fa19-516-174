14e61df629061cf1b540e4de3b6923a1858a998a
sahancha <sancha@iu.edu>
2019-12-20 12:30:50 -0500

    logistic regression

--- project/scikitlogistic/cloudmesh_ai/file.py
+++ project/scikitlogistic/cloudmesh_ai/file.py
@@ -7,13 +7,10 @@ data = db["Dataset"]
 
 def upload(file=None):
 
-    filename = file.filename
-    data.insert_one(filename)
-
-#    if 'file' in request.files:
-#        file = request.files['file']
-#        data.save_file(file.filename, file)
-#        data.insert({'uploadfile_name': file.filename})
+    if 'file' in request.files:
+        file = request.files['file']
+        data.save_file(file.filename, file)
+        data.insert({'uploadfile_name': file.filename})
         #f = data.find_one_or_404({'uploadfile_name': file.filename})
 
     return 'File uploaded successfully'
--- project/scikitlogistic/server.py
+++ project/scikitlogistic/server.py
@@ -1,4 +1,4 @@
-from flask import jsonify, render_template
+from flask import jsonify
 import connexion
 import gridfs
 
