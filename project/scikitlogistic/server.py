1732306508b570b280b49253d5594eecdbc2c29a
sahancha <sancha@iu.edu>
2019-12-20 12:21:14 -0500

    logistic regression

---
+++ project/scikitlogistic/server.py
@@ -0,0 +1,27 @@
+from flask import jsonify, render_template
+import connexion
+import gridfs
+
+# http://0.0.0.0:8080/cloudmesh_ai/ui
+
+from cloudmesh.mongo.CmDatabase import CmDatabase
+
+cmdb = CmDatabase()
+db = cmdb.client["ai_services"]
+
+data = db["files"]
+data.insert_one({"AI Services": "Logistic regression and Image Classification"})
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="../scikitlogistic/")
+app.add_api("api.yaml")
+
+# create a URL route in our application for "/"
+@app.route("/")
+def home():
+    msg = {"msg": "It's working!"}
+    return jsonify(msg)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
